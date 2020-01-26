67ed9f8e210fb1b69461989f137563fa39a0dcda
Desty-3000 <56746369+Desty-3000@users.noreply.github.com>
2020-01-26 19:57:50 +0100

    Add files via upload

--- Mapping.py
+++ Mapping.py
@@ -1,3 +1,24 @@
-﻿
+﻿import pygame
 
-f= open("curMapping.txt","w+")
+
+pygame.init()
+
+
+
+
+Curr_Difficulte = 0
+
+Niveau = 1
+
+Seuil_Difficulte = 0
+
+def Initiate_Mapping_Phase(Seuil_Difficulte,Niveau):
+
+    New_Seuil_Difficulte = 10 + Niveau * 1.5
+    return New_Seuil_Difficulte
+
+Seuil_Difficulte = Initiate_Mapping_Phase(Seuil_Difficulte,Niveau)
+
+def Refresh_Difficulty_Bar():
+    import Main
+    Difficulty_Bar = pygame.draw.rect(Main.fenetre,(255,255,255),(50,50,100,20))
