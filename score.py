7eeb9920920c29e2fa975fd298a49a95be21e0ee
tofus221 <59098530+tofus221@users.noreply.github.com>
2020-01-27 23:00:12 +0100

    Update score.py

--- score.py
+++ score.py
@@ -1,6 +1,14 @@
 import pygame
-def Score_temp():
+import ennemies
+
+def temp():
     while vivant:
         pygame.time.wait(1000)
-        score += 10
+        score_temp += 10
+    return score_temp
 
+def ennemis(liste_killcount,liste_valeur_ennemis):
+    score_ennemis = 0
+    for position,valeur in enumerate(liste_valeur_ennemis):
+        score_ennemis += valeur*liste_killcount[position]
+    return score_ennemis
