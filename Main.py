44097d81da83845d3470f30f8a55fc5fce2b7f43
tofus221 <59098530+tofus221@users.noreply.github.com>
2020-01-30 10:23:42 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
 ﻿import Mapping
+import pygame
+
+pygame.init()
+
+score = 0
+niveau = 0
+
+X = 800
+Y = 600
+
+XMINGRID = 100
+YMINGRID = 100
+
+XMAXGRID = 1000
+YMAXGRID = 1000
+
+
+fenetre = pygame.display.set_mode((X, Y))
+
+
+Jeu = True
+
+while Jeu :
+
+    Mapping.Refresh_Difficulty_Bar(Mapping.Curr_Difficulte,Mapping.Seuil_Difficulte,fenetre)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Jeu = False
+
+    pygame.display.update()
+
+pygame.display.quit()
+pygame.quit()
--- Mapping.py
+++ Mapping.py
@@ -1,24 +1,41 @@
 ﻿import pygame
 
-
 pygame.init()
 
-
-
-
-Curr_Difficulte = 0
+Curr_Difficulte = 8
 
 Niveau = 1
 
-Seuil_Difficulte = 0
 
-def Initiate_Mapping_Phase(Seuil_Difficulte,Niveau):
+class text:
+    def __init__(self,width,text,color1,color2,x,y,fenetre):
+        self.font = pygame.font.Font('freesansbold.ttf', width)
+        self.playBtn = self.font.render(text, True, color1, color2)
+        self.playRect = self.playBtn.get_rect()
+        self.playRect.topleft = (x,y)
+        fenetre.blit(self.playBtn, self.playRect)
 
-    New_Seuil_Difficulte = 10 + Niveau * 1.5
-    return New_Seuil_Difficulte
 
-Seuil_Difficulte = Initiate_Mapping_Phase(Seuil_Difficulte,Niveau)
 
-def Refresh_Difficulty_Bar():
-    import Main
-    Difficulty_Bar = pygame.draw.rect(Main.fenetre,(255,255,255),(50,50,100,20))
+
+
+#Phase 0 = Mapping , 1 = Attacking
+Mapping_Phase = False
+
+Seuil_Difficulte = 10
+
+def Initiate_Mapping_Phase(Seuil_Difficulte,Niveau,Phase):
+    Phase = True
+    New_Seuil_Difficulte = 16 + Niveau * 1.5
+    return New_Seuil_Difficulte,Phase
+
+Seuil_Difficulte,Mapping_Phase = Initiate_Mapping_Phase(Seuil_Difficulte,Niveau,Mapping_Phase)
+
+def Refresh_Difficulty_Bar(Curr_Difficulte,Seuil_Difficulte,fenetre):
+    pygame.draw.rect(fenetre,(105,105,105),(100,20,200,20))
+    pygame.draw.rect(fenetre,(255,255,0),(100,20,200*(Curr_Difficulte/Seuil_Difficulte),20))
+    IntDif = text(20,str(Curr_Difficulte)+"/"+str(Seuil_Difficulte),(255,255,255),(0,0,0),320,20,fenetre)
+
+
+
+
