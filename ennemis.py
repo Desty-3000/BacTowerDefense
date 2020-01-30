4dd5d54f5713849adac3765f911b87379c84beb8
tofus221 <59098530+tofus221@users.noreply.github.com>
2020-01-30 10:10:42 +0100

    Add files via upload

---
+++ ennemis.py
@@ -0,0 +1,9 @@
+import pygame
+
+class ennemis:
+    def __init__(self,vitesse,couleur,vie,element):
+        self.vitesse = vitesse
+        self.couleur = couleur
+        self.vie = vie
+        self.element = element
+
