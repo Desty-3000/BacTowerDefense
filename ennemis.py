0c30731e34f71dcdc3c18b3c4f881c3bf8e93fa6
tofus221 <tofus221@users.noreply.github.com>
2020-01-31 18:58:02 +0100

    Update ennemis.py

--- ennemis.py
+++ ennemis.py
@@ -8,14 +8,19 @@ class types:
         self.element = element
         self.recompense = recompense
 
-def different_ennemis():
-    ennemis_normal = ennemis.types(1,(0,0,255),1,'normal',10)
+def differents_ennemis():
+    ennemis_normal = types(1,(0,0,255),1,'normal',10)
     return ennemis_normal
 
-def spawn_ennemis(type_ennemis,carte,vague):
+def spawn_ennemis(type_ennemis,carte,vague,fenetre):
     for index,liste in enumerate(carte):
         for index2,valeur in enumerate(a):
             if valeur == 98:
                 spawn = [index,index2]          #determine l'index du spawn, ce qui va permetre de calculer les coordonné pour draw les ennemis avec spawn[0] = y et spawn[1] = x
+    ennemis = pygame.draw.rect(fenetre,type_ennemis.couleur,(spawn[1]*50,spawn[0]*50,10,10))
+    ennemis_vivant = []
+    ennemis_vivant.append(ennemis)
+    return spawn
 
-def pathfinding(carte)
+def déplacement(carte,emplacement_spawn):
+
